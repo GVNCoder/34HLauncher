ae5a0d1220699e75c902a743be5d348596357a56
Will Wouod <dmitrievsvp@gmail.com>
2020-07-12 17:21:53 +0300

    Opacity example

---
+++ Launcher.Theme/Controls/Converters/SliderValue2OpacityConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Launcher.XamlThemes.Controls.Converters
+{
+    public class SliderValue2OpacityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var decimalValue = (double) value;
+            return decimalValue / 100;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
