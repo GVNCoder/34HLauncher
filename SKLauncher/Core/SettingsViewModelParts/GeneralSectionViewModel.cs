342a252a5885b54754f143d9b0ec53c9f98aef39
Will Wouod <dmitrievsvp@gmail.com>
2020-07-12 15:27:29 +0300

    ViewModel settings separation

---
+++ SKLauncher/Core/SettingsViewModelParts/GeneralSectionViewModel.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Launcher.Core.Interaction;
+using Launcher.Core.RPC;
+using Launcher.Core.Services;
+using Launcher.Core.Services.Dialog;
+using Launcher.Core.Services.EventLog;
+using Launcher.Core.Shared;
+using Launcher.Helpers;
+using Launcher.Localization.Loc;
+using Launcher.Localization.Loc.inCodeLocalizationMap;
+
+using SLM = Launcher.Localization.Loc.inCodeLocalizationMap.SharedLocalizationMap;
+
+namespace Launcher.Core.SettingsViewModelParts
+{
+    public class GeneralSectionViewModel : UIBaseViewModel
+    {
+        private readonly ITextDialogService _dialogService;
+        private readonly ISettingsService _settingsService;
+        private readonly IEventLogService _eventLog;
+        private readonly IDiscord _discord;
+
+        private LauncherSettings _settings;
+        private bool _isLoaded;
+
+        public GeneralSectionViewModel(
+            ITextDialogService dialogService,
+            ISettingsService settingsService,
+            IEventLogService eventLog,
+            IDiscord discord)
+        {
+            _dialogService = dialogService;
+            _settingsService = settingsService;
+            _eventLog = eventLog;
+            _discord = discord;
+
+            LocalizationEnumerable = new[]
+            {
+                new KeyValuePair<string, LocalizationEnum>("English", LocalizationEnum.EN),
+                new KeyValuePair<string, LocalizationEnum>("Русский", LocalizationEnum.RU),
+            };
+        }
+
+        #region Dependency properties
+
+        public int LocalizationIndex
+        {
+            get => (int)GetValue(LocalizationIndexProperty);
+            set => SetValue(LocalizationIndexProperty, value);
+        }
+        public static readonly DependencyProperty LocalizationIndexProperty =
+            DependencyProperty.Register("LocalizationIndex", typeof(int), typeof(GeneralSectionViewModel), new PropertyMetadata(0, _localizationIndexChangedCallback));
+
+        private static void _localizationIndexChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var viewModel = (GeneralSectionViewModel) d;
+            var value = (int) e.NewValue;
+
+            viewModel._settings.Localization = viewModel.LocalizationEnumerable[value].Value;
+            LocManager.SetLocale(viewModel._settings.Localization);
+        }
+
+        public bool UnfoldGameWindow
+        {
+            get => (bool)GetValue(UnfoldGameWindowProperty);
+            set => SetValue(UnfoldGameWindowProperty, value);
+        }
+        public static readonly DependencyProperty UnfoldGameWindowProperty =
+            DependencyProperty.Register("UnfoldGameWindow", typeof(bool), typeof(GeneralSectionViewModel), new PropertyMetadata(false, _UnfoldGameWindowChangedCallback));
+
+        private static void _UnfoldGameWindowChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var viewModel = (GeneralSectionViewModel) d;
+            var value = (bool) e.NewValue;
+
+            viewModel._settings.UnfoldGameWindow = value;
+        }
+
+        public string ZClientPath
+        {
+            get => (string)GetValue(ZClientPathProperty);
+            set => SetValue(ZClientPathProperty, value);
+        }
+        public static readonly DependencyProperty ZClientPathProperty =
+            DependencyProperty.Register("ZClientPath", typeof(string), typeof(GeneralSectionViewModel), new PropertyMetadata("None", _zClientPathChangedCallback));
+
+        private static void _zClientPathChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var viewModel = (GeneralSectionViewModel) d;
+            var value = (string) e.NewValue;
+
+            viewModel._settings.PathToZClient = value;
+        }
+
+        public bool AutorunZClient
+        {
+            get => (bool)GetValue(AutorunZClientProperty);
+            set => SetValue(AutorunZClientProperty, value);
+        }
+        public static readonly DependencyProperty AutorunZClientProperty =
+            DependencyProperty.Register("AutorunZClient", typeof(bool), typeof(GeneralSectionViewModel), new PropertyMetadata(false, _autorunZClientChangedCallback));
+
+        private static void _autorunZClientChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var viewModel = (GeneralSectionViewModel) d;
+            var value = (bool) e.NewValue;
+
+            viewModel._settings.RunZClient = value;
+        }
+
+        public bool UseDiscordPresence
+        {
+            get => (bool)GetValue(UseDiscordPresenceProperty);
+            set => SetValue(UseDiscordPresenceProperty, value);
+        }
+        public static readonly DependencyProperty UseDiscordPresenceProperty =
+            DependencyProperty.Register("UseDiscordPresence", typeof(bool), typeof(GeneralSectionViewModel), new PropertyMetadata(false, _useDiscordPresenceChangedCallback));
+
+        private static async void _useDiscordPresenceChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var viewModel = (GeneralSectionViewModel) d;
+            var value = (bool) e.NewValue;
+
+            if (value)
+            {
+                viewModel._discord.Start();
+            }
+            else
+            {
+                viewModel._discord.Stop();
+            }
+
+            viewModel._settings.UseDiscordPresence = value;
+            if (viewModel._isLoaded) await _WaitToggleAsync(viewModel);
+        }
+
+        private static async Task _WaitToggleAsync(GeneralSectionViewModel vm)
+        {
+            vm.CanUseDiscordPresence = false;
+            await Task.Delay((int)TimeSpan.FromSeconds(7).TotalMilliseconds);
+            vm.CanUseDiscordPresence = true;
+        }
+
+        public bool TryToConnect
+        {
+            get => (bool)GetValue(TryToConnectProperty);
+            set => SetValue(TryToConnectProperty, value);
+        }
+        public static readonly DependencyProperty TryToConnectProperty =
+            DependencyProperty.Register("TryToConnect", typeof(bool), typeof(GeneralSectionViewModel), new PropertyMetadata(false, TryToConnectPropertyChangedCallback));
+
+        private static async void TryToConnectPropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var viewModel = (GeneralSectionViewModel) d;
+            var value = (bool) e.NewValue;
+
+            if (value && viewModel._isLoaded)
+            {
+                await viewModel._dialogService.OpenDialog(SharedLocalizationMap.TryToConnectHeader, SharedLocalizationMap.TryToConnect,
+                    TextDialogButtons.Ok);
+            }
+
+            viewModel._settings.TryToConnect = value;
+        }
+
+        public bool CanUseDiscordPresence
+        {
+            get => (bool)GetValue(CanUseDiscordPresenceProperty);
+            set => SetValue(CanUseDiscordPresenceProperty, value);
+        }
+        public static readonly DependencyProperty CanUseDiscordPresenceProperty =
+            DependencyProperty.Register("CanUseDiscordPresence", typeof(bool), typeof(GeneralSectionViewModel), new PropertyMetadata(true));
+
+        #endregion
+
+        private void _AssignSettings(LauncherSettings settings)
+        {
+            UnfoldGameWindow = settings.UnfoldGameWindow;
+            LocalizationIndex = Array.FindIndex(LocalizationEnumerable, p => p.Value == settings.Localization);
+            ZClientPath = string.IsNullOrEmpty(settings.PathToZClient)
+                ? ZClientPath
+                : settings.PathToZClient;
+            AutorunZClient = settings.RunZClient;
+            UseDiscordPresence = settings.UseDiscordPresence;
+            TryToConnect = settings.TryToConnect;
+        }
+
+        public KeyValuePair<string, LocalizationEnum>[] LocalizationEnumerable { get; }
+
+        public override ICommand LoadedCommand => new DelegateCommand(obj =>
+        {
+            _settings = _settingsService.GetLauncherSettings();
+            _AssignSettings(_settings);
+            _isLoaded = true;
+        });
+
+        public override ICommand UnloadedCommand => new DelegateCommand(obj => { _isLoaded = false; });
+
+        public ICommand SelectZClientCommand => new DelegateCommand(obj =>
+        {
+            var dialog = SettingsDialogHelper.BuildOpenFileDialog(AppDomain.CurrentDomain.BaseDirectory, SLM.SelectZClientPathHeader,
+                "ZClient file (*.exe)|ZClient.exe");
+            var dlgResult = dialog.ShowDialog();
+
+            if (!dlgResult.GetValueOrDefault()) return;
+
+            ZClientPath = dialog.FileName;
+        });
+
+        public ICommand DetectZClientCommand => new DelegateCommand(obj =>
+        {
+            var process = Process.GetProcessesByName("ZClient").FirstOrDefault();
+            if (process != null)
+            {
+                ZClientPath = process.MainModule.FileName;
+            }
+            else
+            {
+                _eventLog.Log(EventLogLevel.Warning, SLM.DetectingZClientProcessHeader, SLM.DetectingZClientProcess);
+            }
+        });
+    }
+}
--- SKLauncher/ViewModel/SettingsViewModel.cs
+++ SKLauncher/ViewModel/SettingsViewModel.cs
@@ -1,288 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Input;
-using System.Windows;
+﻿using System.Windows.Input;
 using System.Windows.Controls;
-using System.Windows.Media;
 
 using Launcher.Core.Bases;
 using Launcher.Core.Interaction;
 using Launcher.Core.RPC;
 using Launcher.Core.Services;
-using Launcher.Core.Services.Dialog;
 using Launcher.Core.Services.EventLog;
+using Launcher.Core.SettingsViewModelParts;
 using Launcher.Core.Shared;
-using Launcher.Localization.Loc;
-using Launcher.XamlThemes.Theming;
 
-using Microsoft.Win32;
+using Ninject;
 
 using SLM = Launcher.Localization.Loc.inCodeLocalizationMap.SharedLocalizationMap;
-using LM = Launcher.Localization.Loc.LocManager;
-using TM = Launcher.XamlThemes.Theming.ThemeManager;
 
 namespace Launcher.ViewModel
 {
-    public class SettingsViewModel : PageViewModelBase
+    public class SettingsViewModel : PageViewModelBase, IBlurredPage
     {
+        public ThemesSectionViewModel ThemeSectionViewModel { get; }
+        public GeneralSectionViewModel GeneralSectionViewModel { get; }
+
         private readonly ISettingsService _settingsService;
         private readonly IEventLogService _eventLogService;
-        private readonly ITextDialogService _dialogService;
-
-        private LauncherSettings _launcherSettings;
-        private bool _isLoaded;
-
-        private OpenFileDialog _getOpenFileDialog(string initialDirectory, string title, string filter)
-        {
-            var _dialog = new OpenFileDialog();
-
-            // set values
-            _dialog.InitialDirectory = initialDirectory;
-            _dialog.Title = title;
-            _dialog.Filter = filter;
-            _dialog.Multiselect = false;
-            _dialog.CheckFileExists = true;
-            _dialog.CheckPathExists = true;
-            _dialog.FileName = string.Empty;
-
-            return _dialog;
-        }
 
         public SettingsViewModel(
             ISettingsService settingsService,
             IEventLogService eventLogService,
             IUIHostService hostService,
-            ITextDialogService dialogService,
-            IDiscord discord) : base(discord)
+            IDiscord discord,
+            IKernel kernel) : base(discord)
         {
             _settingsService = settingsService;
             _eventLogService = eventLogService;
-            _dialogService = dialogService;
 
-            WindowBackgroundContent = hostService.GetHostContainer(UIElementConstants.HostWindowBackground) as Grid;
+            // vm parts
+            ThemeSectionViewModel = kernel.Get<ThemesSectionViewModel>();
+            GeneralSectionViewModel = kernel.Get<GeneralSectionViewModel>();
 
-            LocalizationEnumerable = new []
-            {
-                new KeyValuePair<string, LocalizationEnum>("English", LocalizationEnum.EN),
-                new KeyValuePair<string, LocalizationEnum>("Русский", LocalizationEnum.RU),
-            };
-            ThemeEnumerable = new []
-            {
-                new KeyValuePair<string, ThemeEnum>("Dark", ThemeEnum.Dark),
-                new KeyValuePair<string, ThemeEnum>("Light", ThemeEnum.Light), 
-            };
+            BackgroundContent = hostService.GetHostContainer(UIElementConstants.HostWindowBackground) as Grid;
         }
 
-        public KeyValuePair<string, LocalizationEnum>[] LocalizationEnumerable { get; }
-        public KeyValuePair<string, ThemeEnum>[] ThemeEnumerable { get; }
-        public Grid WindowBackgroundContent { get; }
-
-        #region Dependency properties
-
-        public int ThemeIndex
-        {
-            get => (int)GetValue(ThemeIndexProperty);
-            set => SetValue(ThemeIndexProperty, value);
-        }
-        public static readonly DependencyProperty ThemeIndexProperty =
-            DependencyProperty.Register("ThemeIndex", typeof(int), typeof(SettingsViewModel), new PropertyMetadata(0, _themeIndexChangedCallback));
-
-        private static void _themeIndexChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            var viewModel = (SettingsViewModel) d;
-            var value = (int) e.NewValue;
-
-            viewModel._launcherSettings.Theme = viewModel.ThemeEnumerable[value].Value;
-            ThemeManager.ApplyTheme(viewModel._launcherSettings.Theme);
-        }
-
-        public KeyValuePair<AccentEnum, Color>? AccentScheme
-        {
-            get => (KeyValuePair<AccentEnum, Color>?)GetValue(AccentSchemeProperty);
-            set => SetValue(AccentSchemeProperty, value);
-        }
-        public static readonly DependencyProperty AccentSchemeProperty =
-            DependencyProperty.Register("AccentScheme", typeof(KeyValuePair<AccentEnum, Color>?), typeof(SettingsViewModel), new PropertyMetadata(null, _accentSchemeChangedCallback));
-
-        private static void _accentSchemeChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            var viewModel = (SettingsViewModel) d;
-            var value = (KeyValuePair<AccentEnum, Color>?) e.NewValue;
-
-            if (value == null) return;
-
-            viewModel._launcherSettings.Accent = value.Value.Key;
-            ThemeManager.ApplyAccent(value.Value.Key);
-        }
-
-        public int LocalizationIndex
-        {
-            get => (int)GetValue(LocalizationIndexProperty);
-            set => SetValue(LocalizationIndexProperty, value);
-        }
-        public static readonly DependencyProperty LocalizationIndexProperty =
-            DependencyProperty.Register("LocalizationIndex", typeof(int), typeof(SettingsViewModel), new PropertyMetadata(0, _localizationIndexChangedCallback));
-
-        private static void _localizationIndexChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            var viewModel = (SettingsViewModel) d;
-            var value = (int) e.NewValue;
-
-            viewModel._launcherSettings.Localization = viewModel.LocalizationEnumerable[value].Value;
-            LocManager.SetLocale(viewModel._launcherSettings.Localization);
-        }
-
-        public bool UnfoldGameWindow
-        {
-            get => (bool)GetValue(UnfoldGameWindowProperty);
-            set => SetValue(UnfoldGameWindowProperty, value);
-        }
-        public static readonly DependencyProperty UnfoldGameWindowProperty =
-            DependencyProperty.Register("UnfoldGameWindow", typeof(bool), typeof(SettingsViewModel), new PropertyMetadata(false, _UnfoldGameWindowChangedCallback));
-
-        private static void _UnfoldGameWindowChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            var viewModel = (SettingsViewModel) d;
-            var value = (bool) e.NewValue;
-
-            viewModel._launcherSettings.UnfoldGameWindow = value;
-        }
-
-        public string ZClientPath
-        {
-            get => (string)GetValue(ZClientPathProperty);
-            set => SetValue(ZClientPathProperty, value);
-        }
-        public static readonly DependencyProperty ZClientPathProperty =
-            DependencyProperty.Register("ZClientPath", typeof(string), typeof(SettingsViewModel), new PropertyMetadata("None", _zClientPathChangedCallback));
-
-        private static void _zClientPathChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            var viewModel = (SettingsViewModel) d;
-            var value = (string) e.NewValue;
-
-            viewModel._launcherSettings.PathToZClient = value;
-        }
-
-        public bool AutorunZClient
-        {
-            get => (bool)GetValue(AutorunZClientProperty);
-            set => SetValue(AutorunZClientProperty, value);
-        }
-        public static readonly DependencyProperty AutorunZClientProperty =
-            DependencyProperty.Register("AutorunZClient", typeof(bool), typeof(SettingsViewModel), new PropertyMetadata(false, _autorunZClientChangedCallback));
-
-        private static void _autorunZClientChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            var viewModel = (SettingsViewModel) d;
-            var value = (bool) e.NewValue;
-
-            viewModel._launcherSettings.RunZClient = value;
-        }
-
-        public bool UseDiscordPresence
-        {
-            get => (bool)GetValue(UseDiscordPresenceProperty);
-            set => SetValue(UseDiscordPresenceProperty, value);
-        }
-        public static readonly DependencyProperty UseDiscordPresenceProperty =
-            DependencyProperty.Register("UseDiscordPresence", typeof(bool), typeof(SettingsViewModel), new PropertyMetadata(false, _useDiscordPresenceChangedCallback));
-
-        private static async void _useDiscordPresenceChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            var viewModel = (SettingsViewModel) d;
-            var value = (bool) e.NewValue;
-
-            if (value)
-            {
-                viewModel._discord.Start();
-            }
-            else
-            {
-                viewModel._discord.Stop();
-            }
-
-            viewModel._launcherSettings.UseDiscordPresence = value;
-            if (viewModel._isLoaded) await _WaitToggleAsync(viewModel);
-        }
-
-        private static async Task _WaitToggleAsync(SettingsViewModel vm)
-        {
-            vm.CanUseDiscordPresence = false;
-            await Task.Delay((int) TimeSpan.FromSeconds(7).TotalMilliseconds);
-            vm.CanUseDiscordPresence = true;
-        }
-
-        public int GameCardOpacity
-        {
-            get => (int)GetValue(GameCardOpacityProperty);
-            set => SetValue(GameCardOpacityProperty, value);
-        }
-        public static readonly DependencyProperty GameCardOpacityProperty =
-            DependencyProperty.Register("GameCardOpacity", typeof(int), typeof(SettingsViewModel), new PropertyMetadata(10, _gameCardOpacityPropertyChangedCallback));
-
-        private static void _gameCardOpacityPropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            var viewModel = (SettingsViewModel) d;
-            var value = (double) ((int) e.NewValue);
-
-            viewModel._launcherSettings.CardTransparency = value / 100;
-        }
-
-        public bool TryToConnect
-        {
-            get => (bool)GetValue(TryToConnectProperty);
-            set => SetValue(TryToConnectProperty, value);
-        }
-        public static readonly DependencyProperty TryToConnectProperty =
-            DependencyProperty.Register("TryToConnect", typeof(bool), typeof(SettingsViewModel), new PropertyMetadata(false, TryToConnectPropertyChangedCallback));
-
-        private static async void TryToConnectPropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            var viewModel = (SettingsViewModel) d;
-            var value = (bool) e.NewValue;
-
-            if (value && viewModel._isLoaded)
-            {
-                await viewModel._dialogService.OpenDialog(SLM.TryToConnectHeader, SLM.TryToConnect,
-                    TextDialogButtons.Ok);
-            }
-
-            viewModel._launcherSettings.TryToConnect = value;
-        }
-
-        public bool CanUseDiscordPresence
-        {
-            get => (bool)GetValue(CanUseDiscordPresenceProperty);
-            set => SetValue(CanUseDiscordPresenceProperty, value);
-        }
-        public static readonly DependencyProperty CanUseDiscordPresenceProperty =
-            DependencyProperty.Register("CanUseDiscordPresence", typeof(bool), typeof(SettingsViewModel), new PropertyMetadata(true));
-
-        #endregion
+        public Grid BackgroundContent { get; }
 
         public override ICommand LoadedCommand => new DelegateCommand(obj =>
         {
             _settingsService.SetGlobalLock();
-            _launcherSettings = _settingsService.GetLauncherSettings();
-
-            UnfoldGameWindow = _launcherSettings.UnfoldGameWindow;
-            LocalizationIndex = Array.FindIndex(LocalizationEnumerable, p => p.Value == _launcherSettings.Localization);
-            ThemeIndex = Array.FindIndex(ThemeEnumerable, p => p.Value == _launcherSettings.Theme);
-            AccentScheme = AccentCollection.Accents.FirstOrDefault(a => a.Key == _launcherSettings.Accent);
-            ZClientPath = string.IsNullOrEmpty(_launcherSettings.PathToZClient)
-                ? ZClientPath
-                : _launcherSettings.PathToZClient;
-            AutorunZClient = _launcherSettings.RunZClient;
-            UseDiscordPresence = _launcherSettings.UseDiscordPresence;
-            TryToConnect = _launcherSettings.TryToConnect;
-            GameCardOpacity = (int) (_launcherSettings.CardTransparency * 100);
-
             _discord.UpdateAFK();
-            _isLoaded = true;
         });
 
         public override ICommand UnloadedCommand => new DelegateCommand(obj =>
@@ -294,55 +57,6 @@ namespace Launcher.ViewModel
             }
 
             _settingsService.FreeGlobalLock();
-            _isLoaded = false;
-        });
-
-        public ICommand SelectZClientCommand => new DelegateCommand(obj =>
-        {
-            var dialog = _getOpenFileDialog(AppDomain.CurrentDomain.BaseDirectory, SLM.SelectZClientPathHeader,
-                "ZClient file (*.exe)|ZClient.exe");
-            var dlgResult = dialog.ShowDialog();
-
-            if (!dlgResult.GetValueOrDefault()) return;
-
-            ZClientPath = dialog.FileName;
-        });
-
-        public ICommand DetectZClientCommand => new DelegateCommand(obj =>
-        {
-            var process = Process.GetProcessesByName("ZClient").FirstOrDefault();
-            if (process != null)
-            {
-                ZClientPath = process.MainModule.FileName;
-            }
-            else
-            {
-                _eventLogService.Log(EventLogLevel.Warning, SLM.DetectingZClientProcessHeader, SLM.DetectingZClientProcess);
-            }
-        });
-
-        public ICommand SelectImageCommand => new DelegateCommand(obj =>
-        {
-            var dialog = _getOpenFileDialog(AppDomain.CurrentDomain.BaseDirectory, SLM.SelectImage,
-                "Image files (*.jpg, *.png, *.bmp)|*.jpg;*.png;*.bmp");
-            var dlgResult = dialog.ShowDialog();
-
-            if (!dlgResult.GetValueOrDefault()) return;
-
-            var result =
-                XamlThemes.Theming.ThemeManager.TrySetBackgroundImage(BackgroundImageEnum.Custom, dialog.FileName);
-            if (!result)
-            {
-                _eventLogService.Log(EventLogLevel.Warning, SLM.CannotSetImageHeader, SLM.CannotSetImage);
-            }
-
-            XamlThemes.Theming.ThemeManager.ApplyBackgroundImage();
-        });
-
-        public ICommand ResetImageCommand => new DelegateCommand(obj =>
-        {
-            XamlThemes.Theming.ThemeManager.TrySetBackgroundImage(BackgroundImageEnum.Default, null);
-            XamlThemes.Theming.ThemeManager.ApplyBackgroundImage();
         });
     }
 }
