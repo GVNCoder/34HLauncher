9dcf9222ec1ca3f6a49ca89eacd8cd1e64d2580c
Will Wouod <dmitrievsvp@gmail.com>
2020-07-13 12:13:22 +0300

    Added some help classes

---
+++ SKLauncher/Styles/PlayerRotation/PlayerViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Zlo4NET.Api.Models.Server;
+
+namespace Launcher.Styles.PlayerRotation
+{
+    public class PlayerViewModel
+    {
+        public PlayerSelectorType Selector { get; }
+        public string Name { get; }
+        public uint Id { get; }
+
+        public PlayerViewModel(PlayerSelectorType selector, ZPlayer player)
+        {
+            Selector = selector;
+            Name = player.Name;
+            Id = player.Id;
+        }
+    }
+}
