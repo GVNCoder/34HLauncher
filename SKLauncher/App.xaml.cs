c216f5ffdace36777aed5cea130b1aa412f72ce5
Will Wouod <dmitrievsvp@gmail.com>
2020-09-12 12:05:19 +0300

    Up-to-date v0.122.1232.0
    Possible disconnect blaze 11-1 fix

--- SKLauncher/App.xaml.cs
+++ SKLauncher/App.xaml.cs
@@ -16,8 +16,8 @@ using Zlo4NET.Api;
 using Zlo4NET.Api.Models.Shared;
 using Zlo4NET.Core.Data;
 
-[assembly: AssemblyVersion("0.122.1231.0")]
-[assembly: AssemblyFileVersion("0.122.1231.0")]
+[assembly: AssemblyVersion("0.122.1232.0")]
+[assembly: AssemblyFileVersion("0.122.1232.0")]
 
 namespace Launcher
 {
