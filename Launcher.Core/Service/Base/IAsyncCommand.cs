be075623facde52355a8e288a4a3f38181183db3
Will Wouod <dmitrievsvp@gmail.com>
2020-09-16 22:00:12 +0300

    Added basic AsyncCommand

---
+++ Launcher.Core/Service/Base/IAsyncCommand.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Launcher.Core.Service.Base
+{
+    public interface IAsyncCommand : ICommand
+    {
+        Task ExecuteAsync(object parameter);
+    }
+}
