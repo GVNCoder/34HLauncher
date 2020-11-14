30ce53667f0b5ec1a81ef25a3c640bd844b67cba
Will Wouod <dmitrievsvp@gmail.com>
2020-11-14 16:37:30 +0200

    Update App.xaml.cs

--- SKLauncher/App.xaml.cs
+++ SKLauncher/App.xaml.cs
@@ -17,7 +17,7 @@ using Zlo4NET.Api;
 using Zlo4NET.Api.Models.Shared;
 using Zlo4NET.Core.Data;
 
-[assembly: AssemblyVersion("0.124.1.2")]
+[assembly: AssemblyVersion("0.124.2.2")]
 
 namespace Launcher
 {
