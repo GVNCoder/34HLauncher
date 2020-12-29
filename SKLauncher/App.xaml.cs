0f66f457c8cf0552451c60887784249f3c63a5a7
Will Wouod <dmitrievsvp@gmail.com>
2020-12-29 19:52:07 +0200

    Up-to-date v0.124.4.2

--- SKLauncher/App.xaml.cs
+++ SKLauncher/App.xaml.cs
@@ -17,7 +17,7 @@ using Zlo4NET.Api;
 using Zlo4NET.Api.Models.Shared;
 using Zlo4NET.Core.Data;
 
-[assembly: AssemblyVersion("0.124.3.2")]
+[assembly: AssemblyVersion("0.124.4.2")]
 
 namespace Launcher
 {
