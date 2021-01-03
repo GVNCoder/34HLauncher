c4f459674c0037dc347b2deb5d8ed56276f4fb7f
Will Wouod <dmitrievsvp@gmail.com>
2021-01-03 19:24:52 +0200

    Implemented Playground game worker

--- SKLauncher/Core/Data/TestRangeGameWorker.cs
+++ SKLauncher/Core/Data/TestRangeGameWorker.cs
@@ -1,24 +1,75 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
-using Launcher.Core.RPC;
+using log4net;
+
 using Launcher.Core.Services;
 using Launcher.Core.Shared;
 
 using Zlo4NET.Api;
+using Zlo4NET.Api.Models.Shared;
 using Zlo4NET.Api.Service;
+using Zlo4NET.Core.Data;
 
 namespace Launcher.Core.Data
 {
     public class TestRangeGameWorker : IGameWorker
     {
+        private readonly ISettingsService _settingsService;
+        private readonly StringBuilder _pipeLogger;
+        private readonly IZApi _zloApi;
+        private readonly ILog _logger;
+
+        private IZGameProcess _gameProcess;
+        private GameSetting _gameSettings;
+
         public TestRangeGameWorker(
-            GameSetting gameSettings,
-            BaseJoinParams param,
-            IZApi api,
-            ISettingsService settingsService,
-            IDiscord discord)
+            ISettingsService settingsService
+            , IZApi zloApi
+            , ILog logger)
         {
+            _settingsService = settingsService;
+            _zloApi = zloApi;
+            _logger = logger;
+
+            _pipeLogger = new StringBuilder();
+        }
+
+        #region IGameWorker
+
+        public async Task Begin(IZGameProcess gameProcess, GameSetting gameSettings)
+        {
+            _gameProcess = gameProcess;
+            _gameSettings = gameSettings;
+
+            // track game pipe
+            _gameProcess.StateChanged += _gamePipeHandler;
+
+            // run game and go to work ;)
+            var runResult = await _gameProcess.RunAsync();
+            if (runResult != ZRunResult.Success)
+            {
+                // log some info
+                var message = $"For {runResult} reason can't run game";
+
+                _logger.Error(message);
+                _OnError(message);
+                _OnWorkComplete();
+            }
+        }
+
+        public void Stop()
+        {
+            var closeResult = _gameProcess.TryClose();
+            if (closeResult) return;
+
+            // log some info
+            const string message = "For some reason can't close game process";
+
+            _logger.Warn(message);
+            _OnError(message);
         }
 
         public event EventHandler<GameWorkerErrorEventArgs> Error;
@@ -27,14 +78,91 @@ namespace Launcher.Core.Data
         public event EventHandler GameLoadingCompleted;
         public event EventHandler Complete;
 
-        public Task Begin(IZGameProcess gameProcess, GameSetting gameSettings)
+        #endregion
+
+        #region Private helpers
+
+        private void _OnWorkComplete() => Complete?.Invoke(this, EventArgs.Empty);
+
+        private void _OnError(string message) => Error?.Invoke(this, new GameWorkerErrorEventArgs(message));
+
+        private void _OnCanCloseGame() => CanCloseGame?.Invoke(this, EventArgs.Empty);
+
+        private void _OnGameLoadingCompleted() => GameLoadingCompleted?.Invoke(this, EventArgs.Empty);
+
+        private void _OnGamePipeLog(string gamePipe) => GamePipe?.Invoke(this, new GamaWorkerPipeLogEventArgs(gamePipe));
+
+        private void _gamePipeHandler(object sender, ZGamePipeArgs e)
         {
-            throw new NotImplementedException();
+            // save part of pipe log
+            _pipeLogger.AppendLine(e.RawFullMessage);
+
+            // handle pipe event
+            switch (e.Event)
+            {
+                case ZGameEvent.Unknown:
+
+                    // ignore but log, it's important to understand, what happen here
+                    _logger.Warn($"Received unknown event with next value {e.RawFullMessage}");
+
+                    break;
+                case ZGameEvent.StateChanged:
+
+                    _handleStates(e.States);
+
+                    break;
+                case ZGameEvent.Alert:
+                    break;
+                case ZGameEvent.GameWaiting:
+
+                    // we're already loaded
+                    _OnGameLoadingCompleted();
+
+                    // inject selected dll`s
+                    if (!_settingsService.GlobalBlock)
+                    {
+                        _zloApi.InjectDll(_gameSettings.Game, _gameSettings.Dlls);
+                    }
+
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
 
-        public void Stop()
+        private void _handleStates(ZGameState[] states)
         {
-            throw new NotImplementedException();
+            // we're have a live game process
+            if (states.Contains(ZGameState.State_GameRun))
+            {
+                _OnCanCloseGame();
+            }
+            // unfold game window
+            else if (states.Contains(ZGameState.State_GameLoading))
+            {
+                var launcherSettings = _settingsService.GetLauncherSettings();
+                // ReSharper disable once InvertIf
+                if (!_settingsService.GlobalBlock && launcherSettings.UnfoldGameWindow)
+                {
+                    var unfoldResult = _gameProcess.TryUnfoldGameWindow();
+                    // ReSharper disable once InvertIf
+                    if (!unfoldResult)
+                    {
+                        const string message = "For some reason can't unfold game window";
+
+                        _OnError(message);
+                        _logger.Warn(message);
+                    }
+                }
+            }
+            else if (states.Contains(ZGameState.State_GameClose))
+            {
+                _OnGamePipeLog(_pipeLogger.ToString());
+                _OnWorkComplete();
+            }
         }
+
+        #endregion
     }
 }
